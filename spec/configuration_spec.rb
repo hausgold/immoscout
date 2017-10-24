9c383a1fdd65badce291f31901694d703251251d
Marcus Geissler <marcus3006@gmail.com>
2017-10-24 22:20:13 +0200

    fix specs and warning

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -5,6 +5,11 @@ require "spec_helper"
 RSpec.describe Immoscout::Configuration do
   let(:instance) { described_class.new }
 
+  before do
+    instance.oauth_token = nil
+    instance.use_sandbox = nil
+  end
+
   it "can set oauth_token" do
     expect { instance.oauth_token = 'token' }.to \
       change { instance.oauth_token }.to('token')
--- spec/immoscout_spec.rb
+++ spec/immoscout_spec.rb
@@ -4,6 +4,11 @@ require "spec_helper"
 
 RSpec.describe Immoscout do
   describe '.configure' do
+    before do
+      described_class.configuration.oauth_token = nil
+      described_class.configuration.use_sandbox = nil
+    end
+
     it 'sets oauth_token' do
       expect { described_class.configure { |c| c.oauth_token = 'token' } }.to \
         change { described_class.configuration.oauth_token }.to('token')
