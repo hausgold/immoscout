f3d20ed9378c5125036fdcd36a4331a123237ac8
Marcus Geissler <marcus3006@gmail.com>
2017-11-14 14:54:04 +0100

    rubocop fixes

--- lib/immoscout/configuration.rb
+++ lib/immoscout/configuration.rb
@@ -14,10 +14,12 @@ module Immoscout
     config_accessor(:api_version) { "v1.0" }
 
     config_accessor(:api_url_live) do
-      "https://rest.immobilienscout24.de/restapi/api/offer/#{api_version}"
+      "https://rest.immobilienscout24.de/" \
+      "restapi/api/offer/#{api_version}"
     end
     config_accessor(:api_url_sandbox) do
-      "https://rest.sandbox-immobilienscout24.de/restapi/api/offer/#{api_version}"
+      "https://rest.sandbox-immobilienscout24.de/" \
+      "restapi/api/offer/#{api_version}"
     end
   end
 end
