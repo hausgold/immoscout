6fcecdd366c97b919de1bd5e7dfd50da984fe311
Hermann Mayer <hermann.mayer92@gmail.com>
2023-01-18 08:21:55 +0100

    [Release] Bumped the version to 1.4.0.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-### next
+### 1.4.0
 
 * Bundler >= 2.3 is from now on required as minimal version (#10)
 * Dropped support for Ruby < 2.5 (#10)
--- lib/immoscout/version.rb
+++ lib/immoscout/version.rb
@@ -3,7 +3,7 @@
 # The gem version details.
 module Immoscout
   # The version of the +immoscout+ gem
-  VERSION = '1.3.2'
+  VERSION = '1.4.0'
 
   class << self
     # Returns the version of gem as a string.
