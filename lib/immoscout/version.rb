d985713b5e101e186f51ee5832ec27fd782e8dc3
deployhausgold <deploy@hausgold.de>
2023-02-24 11:23:28 +0000

    [Release] Bumped the version to 1.5.0.

--- lib/immoscout/version.rb
+++ lib/immoscout/version.rb
@@ -3,7 +3,7 @@
 # The gem version details.
 module Immoscout
   # The version of the +immoscout+ gem
-  VERSION = '1.4.0'
+  VERSION = '1.5.0'
 
   class << self
     # Returns the version of gem as a string.
